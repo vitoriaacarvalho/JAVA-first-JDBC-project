002234403dc22ac376f0db4472e055247fb9c6fc
vitoria <vitoriabccorrea@gmail.com>
2022-07-15 19:19:17 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,5 @@
 package application;
+import java.util.Date;
 import java.util.List;
 
 import dao.DaoFactory;
@@ -12,11 +13,41 @@ public class Program {
 		SellerDao sellerDao=DaoFactory.createSellerDao();
 		Department department=new Department(2,null);
 		List<Seller> list=sellerDao.findDepartment(department);
+		
 		System.out.println("seller findAll teste");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println("seller insert teste");
+		
+		Seller newSeller=new Seller(null, "Greg", "greg@gmail.com",new Date(), 300.0, department);
+		
+		sellerDao.insert(newSeller);
+		System.out.println("seller inserted into database! new id= "+newSeller.getId());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
